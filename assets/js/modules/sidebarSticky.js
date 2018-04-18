d07c3385e4efd8eb94516ea586fe6f8c2d8f9202
ninja <deedededede@gmail.com>
2018-04-18 17:37:50 +0300

    realy realy end news

--- assets/js/modules/sidebarSticky.js
+++ assets/js/modules/sidebarSticky.js
@@ -1,16 +1,42 @@
 export function sidebarSticky() {
     let sidebar = $('.sidebar');
+    let winHeight = $(window).innerHeight();
+    let footer = $('.footer');
+    let footerHeight = footer.innerHeight();
     function scrollLoad() {
         if ($(window).innerWidth() >= 1200) {
             let scrollTop = $(window).scrollTop();
             if (sidebar.offset().top <= scrollTop) {
                 sidebar.addClass('fixed');
                 sidebar.find('.sidebar-box').css('left', sidebar.offset().left);
+                if(scrollTop + sidebar.find('.sidebar-box').innerHeight() < footer.offset().top ){
+                    sidebar.find('.sidebar-box').css({
+                        'top': '0px',
+                        'bottom':'auto'
+                    })
+                }else{
+                    let bot = -(footer.offset().top - scrollTop - winHeight);
+                    console.log(scrollTop);
+                    console.log(winHeight);
+                    console.log(footerHeight);
+                    console.log(footer.offset().top );
+                    sidebar.find('.sidebar-box').css({
+                        'top': 'auto',
+                        // 'bottom': bot+'px'
+                        'bottom': bot+'px'
+                    })
+                }
             }else{
                 sidebar.removeClass('fixed');
+                sidebar.find('.sidebar-box').css({
+                    'bottom': 'auto',
+                })
             }
         }else{
             sidebar.removeClass('fixed');
+            sidebar.find('.sidebar-box').css({
+                'bottom': 'auto',
+            })
         }
     }
     window.addEventListener("scroll", () => {
