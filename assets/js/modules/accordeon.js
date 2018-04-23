fd03bf67d106c857ff726f91995cee4b4736002d
Eugene Zolotarev <eugene.z@bigdropinc.com>
2018-04-23 16:18:13 +0300

    developers portal

--- assets/js/modules/accordeon.js
+++ assets/js/modules/accordeon.js
@@ -2,21 +2,34 @@ export function accordeon() {
 	let accordeon = $('.accordeon');
 	accordeon.on('click', '.title', function () {
 		let $this = $(this);
-		if ($this.next('.acc-holder').length) {
-			if ($this.hasClass('active')) {
-				$this.removeClass('active');
-				$this.siblings('.acc-holder').slideUp();
-			} else {
-				$('.accordeon .title').removeClass('active');
-				$('.accordeon .acc-holder').slideUp();
+		if ($this.hasClass('active')) {
+			$this.removeClass('active');
+			$this.siblings('.acc-holder').slideUp();
+		} else {
+			$('.accordeon .title').removeClass('active');
+			$('.accordeon .acc-holder').slideUp();
+			if ($this.next('.acc-holder').length) {
 				$this.addClass('active');
 				$this.siblings('.acc-holder').slideDown();
 			}
 		}
-		if ($this.parents('.sidebar')) {
-			$('body, html').animate({
-				scrollTop: 0
-			}, 800);
+	});
+	$('.anchor-list .title, .anchor-list  a').on('click', function () {
+		let targetAttr = $(this).data('anchor');
+		if (targetAttr !== undefined) {
+			$('.developers-portal').find('h2').each(function () {
+				if ($(this).data('anchor') === targetAttr) {
+					let translate = 10;
+					if (!$(this).hasClass('animate') && window.innerWidth > 1024) {
+						translate = 110;
+					}
+					$('body, html').animate({
+						scrollTop: $(this).offset().top - translate
+					}, 800);
+				}
+			});
+			return false;
 		}
 	});
+
 }
