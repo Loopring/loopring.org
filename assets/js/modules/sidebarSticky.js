7345a8d89d2a443fc88d856b11f69389b70111bf
ninja <deedededede@gmail.com>
2018-04-30 19:15:02 +0300

    bugs

--- assets/js/modules/sidebarSticky.js
+++ assets/js/modules/sidebarSticky.js
@@ -1,25 +1,24 @@
 export function sidebarSticky() {
     let sidebar = $('.sidebar');
     let winHeight = $(window).innerHeight();
-    let footer = $('.footer');
-    let footerHeight = footer.innerHeight();
+    let bottomBlock = $('.footer');
+    if($('.partners-logos ').length){
+        bottomBlock = $('.partners-logos ');
+    }
+    // let footerHeight = footer.innerHeight();
     function scrollLoad() {
         if ($(window).innerWidth() >= 1200) {
             let scrollTop = $(window).scrollTop();
             if (sidebar.offset().top <= scrollTop) {
                 sidebar.addClass('fixed');
                 sidebar.find('.sidebar-box').css('left', sidebar.offset().left);
-                if(scrollTop + sidebar.find('.sidebar-box').innerHeight() < footer.offset().top ){
+                if(scrollTop + sidebar.find('.sidebar-box').innerHeight() < bottomBlock.offset().top ){
                     sidebar.find('.sidebar-box').css({
                         'top': '0px',
                         'bottom':'auto'
                     })
                 }else{
-                    let bot = -(footer.offset().top - scrollTop - winHeight);
-                    console.log(scrollTop);
-                    console.log(winHeight);
-                    console.log(footerHeight);
-                    console.log(footer.offset().top );
+                    let bot = -(bottomBlock.offset().top - scrollTop - winHeight);
                     sidebar.find('.sidebar-box').css({
                         'top': 'auto',
                         'bottom': bot+'px'
