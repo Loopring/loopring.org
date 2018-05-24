8623e664363e925da0758182c32c00b6cf4bd77b
wangdong <wangdong@zhongan.io>
2018-05-24 22:06:56 +0800

    disable youtube videos

--- assets/js/modules/globals.js
+++ assets/js/modules/globals.js
@@ -5,7 +5,7 @@ export const constants = {
     }() : function() {
         // document.body.classList.add("no-touch");
         return false;
-    }() ,
+    }(),
     body: $("body")
 };
 export function afteLoads() {
@@ -18,7 +18,7 @@ export function afteLoads() {
             'bottom': '100%',
             'left': 0
         });
-        setTimeout(function () {
+        setTimeout(function() {
             $('.preload-images').remove();
         }, 1000);
         $('html').addClass('no-touch');
@@ -26,37 +26,33 @@ export function afteLoads() {
     window.addEventListener("load", () => {
         this.body.addClass('load');
         $('.preloader').removeClass('loading');
-        setTimeout(function () {
+        setTimeout(function() {
             $('.preloader-wrapper').fadeOut(600)
         }, 500)
     });
-
-
-
-
 }
 export function btnDecorate() {
     $('.btn, input[type="submit"], button').append('<span class="decor-top"></span><span class="decor-bot"></span>')
 }
 export function headerActivities() {
-    $('.btn-menu').on('click', function () {
+    $('.btn-menu').on('click', function() {
         let $this = $(this);
         $this.closest('.header').toggleClass('opened');
         $this.toggleClass('active');
         $this.closest('.header').find('nav').fadeToggle();
         $('body').toggleClass('ovh');
     });
-    $('.text-holder-desktop, .text-holder-mobile').on('click', function (e) {
+    $('.text-holder-desktop, .text-holder-mobile').on('click', function(e) {
         $(this).siblings('.drop-down').fadeToggle();
         $(this).parent().toggleClass('active');
     });
-    $(document).on('click', function (e) {
+    $(document).on('click', function(e) {
         if (!$(e.target).closest('.lang-box').length) {
             $('.drop-down').fadeOut();
             $('.lang-box').removeClass('active')
         }
     });
-    $('.btn-close').on('click', function () {
+    $('.btn-close').on('click', function() {
         $(this).closest('.drop-down').fadeOut();
         $('.lang-box').removeClass('active')
     });
